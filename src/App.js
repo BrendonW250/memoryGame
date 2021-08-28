779ab77d960fa790d5b39087ec37ce227c25c7a5
BrendonW250 <brendonwright16@gmail.com>
2021-08-28 19:21:49 -0400

    home page for memory game

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { render } from 'react-dom';
+import React from 'react';
+import './App.css';
+import Home from './Home/home';
+
+function App() {
+    return(
+      <Home/>
+    )
+    
+  
+  
+    
+  
+}
+
+export default App;
